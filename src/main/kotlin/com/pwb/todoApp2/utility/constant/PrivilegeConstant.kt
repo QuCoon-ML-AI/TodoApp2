69109000b627d21116d593f32916bd1cc45f3950
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-21 15:53:19 +0100

    Initial commit for TodoApp2

---
+++ src/main/kotlin/com/pwb/todoApp2/utility/constant/PrivilegeConstant.kt
@@ -0,0 +1,11 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-21T14:52:10.475281600
+package com.pwb.todoApp2.utility.constant
+
+object PrivilegeConstant {
+    const val CREATE = "CREATE"
+    const val UPDATE = "UPDATE"
+    const val READ = "READ"
+    const val DELETE = "DELETE"
+    const val CHECKER = "CHECKER"
+    const val ALL = "*"
+}
