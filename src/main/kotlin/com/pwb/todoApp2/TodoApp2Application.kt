0cf9d47d3615282dea20fa069d2d26de7aaa6c73
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-21 15:53:17 +0100

    Initial commit for TodoApp2

---
+++ src/main/kotlin/com/pwb/todoApp2/TodoApp2Application.kt
@@ -0,0 +1,13 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-21T14:52:10.475281600
+package com.pwb.todoApp2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoApp2Application
+
+fun main(args: Array<String>) {
+	runApplication< TodoApp2Application>(*args)
+}
+
