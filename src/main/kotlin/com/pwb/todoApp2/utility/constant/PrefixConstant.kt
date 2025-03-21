cc3309b0e418e1bc6257ce4374ee0dc0f5b1eedd
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-21 15:53:18 +0100

    Initial commit for TodoApp2

---
+++ src/main/kotlin/com/pwb/todoApp2/utility/constant/PrefixConstant.kt
@@ -0,0 +1,6 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-21T14:52:10.475281600
+package com.pwb.todoApp2.utility.constant
+
+object PrefixConstant {
+    const val ENTITY = "DBO."
+}
