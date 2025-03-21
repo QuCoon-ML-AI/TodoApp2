2354a1698229194ee479b1ac39d45c44814de273
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-21 15:53:25 +0100

    Initial commit for TodoApp2

---
+++ src/test/kotlin/com/pwb/todoApp2/TodoApp2ApplicationTests.kt
@@ -0,0 +1,15 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-21T14:52:10.506668600
+package com.pwb.todoApp2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoApp2ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
